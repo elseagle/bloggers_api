fa2fef280259180d78905e72809d06836baaeced
elseagle <dowolebolu@gmail.com>
2018-09-18 10:46:19 +0100

    bug fix

--- bloggers_project/bloggers_project/wsgi.py
+++ bloggers_project/bloggers_project/wsgi.py
@@ -11,7 +11,7 @@ import os
 from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+#sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 application = DjangoWhiteNoise(application)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bloggers_project.settings")
 application = get_wsgi_application()

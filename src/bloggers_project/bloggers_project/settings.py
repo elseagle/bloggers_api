c5b0b65aa55d554b441fbb33b038181a1ddbea7b
elseagle <dowolebolu@gmail.com>
2018-09-19 07:01:55 +0100

    bug fix

--- src/bloggers_project/bloggers_project/settings.py
+++ src/bloggers_project/bloggers_project/settings.py
@@ -59,7 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'bloggers_project.urls'
+ROOT_URLCONF = 'bloggers_project.bloggers_project.urls'
 
 TEMPLATES = [
     {
